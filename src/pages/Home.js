03d0b2e9cd40f91707a5e7869e2fbfb62af76797
joao-montanari <joao.unipbr@gmail.com>
2023-01-27 09:26:35 -0300

    estilização da página de login

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function Home() {
-    return (
-        <View style={styles.container}>
-          <Text>Home</Text>
-        </View>
-      );
-    }
-    
+  return (
+      <View style={styles.container}>
+        <Text>Home</Text>
+      </View>
+  );
+}
+
 const styles = StyleSheet.create({
-    container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    },
+  container: {
+  flex: 1,
+  backgroundColor: '#fff',
+  alignItems: 'center',
+  justifyContent: 'center',
+  },
 });
-    
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,18 +1,83 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { useState } from 'react';
 
 export default function Login(){
-    return (
-        <View style={styles.container}>
-          <Text>Login</Text>
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  return (
+      <>
+        <View style={styles.header}>
+          <Text style={styles.textheader}>
+            Login
+          </Text>
         </View>
-      );
-    }
-    
+        <View style={styles.container}>
+          <View style={styles.child}>
+            <TextInput
+              style = {styles.input}
+              placeholder = 'Usuário'
+              keyboardType = 'text'
+              value = {username}
+              onChangeText = {(text) => setUsername(text)}
+            />
+            <TextInput
+              style = {styles.input}
+              placeholder = 'Senha'
+              keyboardType = 'text'
+              value = {password}
+              secureTextEntry = {true}
+              onChangeText = {(text) => setPassword(text)}
+            />
+          </View>
+          <TouchableOpacity style = {styles.btn}>
+            <Text style = {styles.btntext}>
+              ENTRAR
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </>
+    );
+}
+
 const styles = StyleSheet.create({
-    container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  container: {
+    flex: 0.95,
+    backgroundColor: '#efeef5',
     alignItems: 'center',
     justifyContent: 'center',
-    },
+  },
+  child: {
+    width: '100%',
+    alignItems: 'center'
+  },
+  input: {
+    height: 40,
+    margin: 16,
+    padding: 5,
+    width: '80%',
+    backgroundColor: '#fff',
+    borderRadius: 8,
+  },
+  btn: {
+    width: 80,
+    height: 40,
+    borderRadius: 3,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#1b98e0'
+  },
+  btntext: {
+    color: 'white'
+  },
+  header: {
+    flex: 0.05,
+    justifyContent: 'center',
+    backgroundColor: 'white'
+  },
+  textheader: {
+    paddingLeft: 15,
+    fontWeight: 'bold',
+    fontSize: 'large'
+  }
 });
